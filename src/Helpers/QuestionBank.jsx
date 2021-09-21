f0ce66575cd229d58136682d9053cde3653c76b7
Tiago Ferreira <tiagofqaferreira@gmail.com>
2021-09-21 15:02:52 +0200

    firstcommit

---
+++ src/Helpers/QuestionBank.jsx
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "What is 1+3?",
+    optionA: "24",
+    optionB: "21",
+    optionC: "4",
+    optionD: "2",
+    answer: "C",
+  },
+  {
+    prompt: "What is 1+4?",
+    optionA: "24",
+    optionB: "21",
+    optionC: "4",
+    optionD: "5",
+    answer: "D",
+  },
+  {
+    prompt: "What is 1+2?",
+    optionA: "24",
+    optionB: "3",
+    optionC: "2",
+    optionD: "4",
+    answer: "B",
+  },
+];
